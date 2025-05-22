2362de36ac04d9e1688d56624268ffdd1aac1d89
SimlaiRounak <simlairounak@gmail.com>
2025-05-22 11:52:12 +0530

    fix

--- middlewares/requestLogger.js
+++ middlewares/requestLogger.js
@@ -5,19 +5,17 @@
  * @param {Object} res - Express response object used to return reservations
  * @param {Object} next - Express next object used to continue on the request flow
  */
-const requestLogger = () => {
-    return (req, res, next) => {
-        const startTime = process.hrtime();
+const requestLogger = () => (req, res, next) => {
+    const startTime = process.hrtime();
 
-        res.on('finish', () => {
-            const [seconds, nanoseconds] = process.hrtime(startTime);
-            const durationMs = Math.round((seconds * 1e9 + nanoseconds) / 1e6);
+    res.on('finish', () => {
+        const [seconds, nanoseconds] = process.hrtime(startTime);
+        const durationMs = Math.round((seconds * 1e9 + nanoseconds) / 1e6);
 
-            console.log(`${req.method} ${req.originalUrl || req.url} - ${durationMs}ms`);
-        });
+        console.log(`${req.method} ${req.originalUrl || req.url} - ${durationMs}ms`);
+    });
 
-        next();
-    };
+    next();
 };
 
 module.exports = { requestLogger };
